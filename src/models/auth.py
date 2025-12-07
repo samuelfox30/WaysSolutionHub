4676520078c2d002fdaea8f946f1bb4774cb0198
conversaox <samuel@conversaox.com.br>
2025-12-07 12:26:02 -0300

    atualizações necessárias para subir o site

--- src/models/auth.py
+++ src/models/auth.py
@@ -2,11 +2,18 @@ import mysql.connector
 from mysql.connector import errorcode
 
 # Credenciais do banco de dados centralizadas
+# DB_CONFIG = {
+#     'host': "localhost",
+#     'user': "root",
+#     'password': "root",
+#     'database': "WaysDb"
+# }
+
 DB_CONFIG = {
-    'host': "localhost",
-    'user': "root",
-    'password': "root",
-    'database': "WaysDb"
+    'host': "mysql.wayssolution.kinghost.net",
+    'user': "wayssolution",
+    'password': "DS6tgb8yaHB67",
+    'database': "wayssolution"
 }
 
 class DatabaseConnection:
